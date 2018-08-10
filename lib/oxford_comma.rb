22833c10b74604590c383c408bd835da3e2f6655
Brad McCollum <btmcco8@gmail.com>
2018-08-10 22:40:53 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,11 +5,11 @@ def oxford_comma(array)
   #   array.join(" and ")
   # else array.length >= 3
   #   ajoin(", ")
-  if array.length == 1
-    array.join
-  elsif array.length == 2
-    array.join(" and ")
-  else
+  # if array.length == 1
+  #   array.join
+  # elsif array.length == 2
+  #   array.join(" and ")
+  # else
     array[0..-2].join(", ") << ", and " << array[-1]
-end
+ end
 end
