3ea1e3d8f4319497f7dd6e39a881109a349d0edd
Brad McCollum <btmcco8@gmail.com>
2018-08-10 22:34:04 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,8 +5,13 @@ def oxford_comma(array)
   #   array.join(" and ")
   # else array.length >= 3
   #   ajoin(", ")
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
     new_array = array.map {|word| "#{word} ,"}
-    output = new_array.join(", and")
+    output = new_array.join(", and ")
     output
 
 end
