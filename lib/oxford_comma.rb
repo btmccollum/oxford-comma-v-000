10d61c56ec04da4a9d8137547ff56c9ff0a11cf2
Brad McCollum <btmcco8@gmail.com>
2018-08-10 22:39:11 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -10,8 +10,7 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   else
-    new_array = array.map {|word| "#{word} ,"}
-    output[0..-2] = new_array.join(", ") << ", and " << output[-1]
+    array[0..-2].join(", ") << ", and " << array[-1]
     output
 end
 end
